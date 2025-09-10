df0aba6c83861279b65674a93741f0041c4bbb5c
juanlechuga <juanlechuga@wolfsellers.com>
2025-09-10 13:15:52 -0600

    Create telcel-card-pack.js

---
+++ blocks/telcel-card-pack/telcel-card-pack.js
@@ -0,0 +1,6 @@
+// blocks/telcel-card-pack/telcel-card-pack.js
+// Bloque vacío: sólo registra la clase para confirmar carga.
+export default function decorate(block) {
+  block.classList.add('telcel-card-pack');
+  // no renderizamos nada todavía
+}
